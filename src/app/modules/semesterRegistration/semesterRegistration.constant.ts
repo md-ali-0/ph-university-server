8ce75b9628d8abc585d4a41fce0f739fdc5cfe82
md-ali-0 <mohammad.98482@gmail.com>
2024-06-10 08:58:21 +0600

    Semester Registration Updated

--- src/app/modules/semesterRegistration/semesterRegistration.constant.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.constant.ts
@@ -1,1 +1,7 @@
 export const semesterRegistrationStatus = ['upcoming' , 'ongoing' , 'ended'];
+
+export const RegistrationStatus = {
+    upcoming: 'upcoming',
+    ongoing: 'ongoing',
+    ended: 'ended'
+} as const
--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,24 +1,48 @@
-import { CONFLICT, NOT_FOUND } from 'http-status';
+import { BAD_REQUEST, CONFLICT, NOT_FOUND } from 'http-status';
+import { Types } from 'mongoose';
 import QueryBuilder from '../../builder/QueryBuilder';
 import AppError from '../../error/AppError';
-import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { RegistrationStatus } from './semesterRegistration.constant';
 import { ISemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistration } from './semesterRegistration.model';
 
 const createSemesterRegistration = async (
     payload: ISemesterRegistration,
 ): Promise<ISemesterRegistration> => {
+    /**
+     * Step1: Check if there any registered semester that is already 'UPCOMING'|'ONGOING'
+     * Step2: Check if the semester is exist
+     * Step3: Check if the semester is already registered!
+     * Step4: Create the semester registration
+     */
+
     const academicSemester = payload.academicSemester;
-    if (academicSemester) {
-        const isAcademicSemesterExists = await AcademicSemester.findById(
-            payload.academicSemester,
+
+    // check if their any semester upcoming
+
+    const isAcademicSemesterExists = await SemesterRegistration.findOne({
+        $or: [{ status: RegistrationStatus.upcoming }, { status: RegistrationStatus.ongoing }],
+    });
+
+    if (isAcademicSemesterExists) {
+        throw new AppError(
+            BAD_REQUEST,
+            `Their is already a ${payload.status} register semester !`,
         );
-        if (!isAcademicSemesterExists)
-            throw new AppError(NOT_FOUND, 'Academic Semester not found');
     }
+
+    const isSemesterRegistrationExists =
+        await SemesterRegistration.findById(academicSemester);
+
+    const currentSemesterStatus = isSemesterRegistrationExists?.status;
+    const requestedStatus = payload?.status;
+
+    if (!isSemesterRegistrationExists)
+        throw new AppError(NOT_FOUND, 'Academic Semester not found');
+
     if (academicSemester) {
         const isSemesterRegistrationExists = await SemesterRegistration.findOne(
-            { academicSemester: payload.academicSemester },
+            { academicSemester },
         );
         if (isSemesterRegistrationExists)
             throw new AppError(
@@ -26,6 +50,29 @@ const createSemesterRegistration = async (
                 'Semester Registration Already Exists',
             );
     }
+
+    if (currentSemesterStatus === RegistrationStatus.ended) {
+        throw new AppError(
+            BAD_REQUEST,
+            `This semester is already ${currentSemesterStatus}`,
+        );
+    }
+
+    // UPCOMING --> ONGOING --> ENDED
+    if (currentSemesterStatus === RegistrationStatus.upcoming && requestedStatus === RegistrationStatus.ended) {
+        throw new AppError(
+            BAD_REQUEST,
+            `You can not directly change status from ${currentSemesterStatus} to ${requestedStatus}`,
+        );
+    }
+
+    if (currentSemesterStatus === RegistrationStatus.ongoing && requestedStatus === RegistrationStatus.upcoming) {
+        throw new AppError(
+            BAD_REQUEST,
+            `You can not directly change status from ${currentSemesterStatus} to ${requestedStatus}`,
+        );
+    }
+
     const semesterRegistration = await SemesterRegistration.create(payload);
     return semesterRegistration;
 };
@@ -34,6 +81,15 @@ const updateSemesterRegistration = async (
     id: string,
     payload: Partial<ISemesterRegistration>,
 ): Promise<ISemesterRegistration | null> => {
+    // is the requested semester is ended . we will not updated
+    const isRequestedSemesterEnded = await SemesterRegistration.findOne({
+        _id: new Types.ObjectId(id),
+        status: { $eq: RegistrationStatus.ended },
+    });
+
+    if (isRequestedSemesterEnded)
+        throw new AppError(BAD_REQUEST, `This semester is already ended`);
+
     const semesterRegistration = await SemesterRegistration.findByIdAndUpdate(
         id,
         payload,
